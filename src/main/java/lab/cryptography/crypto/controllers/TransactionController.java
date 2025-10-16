fb7911178e7627534036c63aeb91db14ecb1b3cc
Luizhnrs <Luizhnrsg@gmail.com>
2025-10-16 00:13:53 -0300

    FEAT: Transaction Controller -> PostMapping and GetMapping implemented

---
+++ src/main/java/lab/cryptography/crypto/controllers/TransactionController.java
@@ -0,0 +1,35 @@
+package lab.cryptography.crypto.controllers;
+
+import lab.cryptography.crypto.controllers.dto.request.CreateTransactionRequest;
+import lab.cryptography.crypto.controllers.dto.response.TransactionResponse;
+import lab.cryptography.crypto.service.TransactionService;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TransactionController {
+    private final TransactionService service;
+    private final ServerProperties serverProperties;
+
+    public TransactionController(TransactionService service, ServerProperties serverProperties) {
+        this.service = service;
+        this.serverProperties = serverProperties;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create (@RequestBody CreateTransactionRequest request){
+        service.create(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TransactionResponse>> listAll (@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                                              @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize){
+        var response = service.listAll(page, pageSize);
+        return ResponseEntity.ok(response);
+    }
+
+
+}

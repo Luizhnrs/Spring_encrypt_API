5b86a8e1b1a3e9add4b21f86106e7b6fb5ec53ff
Luizhnrs <Luizhnrsg@gmail.com>
2025-10-16 23:30:18 -0300

    FEAT: Update and Delete Mapping implemented

--- src/main/java/lab/cryptography/crypto/controllers/TransactionController.java
+++ src/main/java/lab/cryptography/crypto/controllers/TransactionController.java
@@ -1,6 +1,7 @@
 package lab.cryptography.crypto.controllers;
 
 import lab.cryptography.crypto.controllers.dto.request.CreateTransactionRequest;
+import lab.cryptography.crypto.controllers.dto.request.UpdateTransactionRequest;
 import lab.cryptography.crypto.controllers.dto.response.TransactionResponse;
 import lab.cryptography.crypto.service.TransactionService;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
@@ -26,17 +27,32 @@ public class TransactionController {
         return ResponseEntity.ok().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update (@PathVariable(value = "id") Long id,
+                                        @RequestBody UpdateTransactionRequest request){
+
+        service.update(id, request);
+        return ResponseEntity.ok().build();
+    }
+
     @GetMapping
     public ResponseEntity<Page<TransactionResponse>> listAll (@RequestParam(name = "page", defaultValue = "0") Integer page,
                                                               @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize){
-        var response = service.listAll(page, pageSize);
-        return ResponseEntity.ok(response);
+        var listResponse = service.listAll(page, pageSize);
+        return ResponseEntity.ok(listResponse);
     }
 
     @GetMapping("/id")
     public ResponseEntity<TransactionResponse> findById(@PathVariable(value = "id") Long id){
-        var response = service.findById(id);
-        return ResponseEntity.ok(response);
+        var findByIdResponse = service.findById(id);
+        return ResponseEntity.ok(findByIdResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (@PathVariable(value = "id") Long id){
+
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
